187ab8116d9f8f57c55952df1843bcba1ed57643
Krymchuc <khrystyna.krymchuk@oa.edu.ua>
2023-05-03 17:12:58 +0300

    Class and design

---
+++ LiteDB_Demo/LiteDB_Demo/Form1.cs
@@ -0,0 +1,46 @@
+﻿using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LiteDB;
+
+namespace LiteDB_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LiteDatabase db;
+            LiteCollection<Contact> list;
+            db = new LiteDatabase($"{Application.StartupPath}\\mydb.db");
+            list = (LiteCollection<Contact>)db.GetCollection<Contact>();
+            //list.EnsureIndex("Id", true);
+            if (list.Count() == 0)
+                contactBindingSource.DataSource = new List<Contact>();
+            else
+                contactBindingSource.DataSource = list.FindAll();
+        }
+
+        private void buttonNew_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonInsert_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
